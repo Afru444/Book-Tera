58bc4b16568e5bf7af9877c2d4c520881c4db39d
Afru444 <afrinshaik2023@gmail.com>
2026-03-25 18:07:44 +0530

    final fixes: S3 + ALB + removed data blocks

--- ec2/alb.tf
+++ ec2/alb.tf
@@ -1,13 +1,26 @@
+# -------------------------------
+# Application Load Balancer (ALB)
+# -------------------------------
 resource "aws_lb" "alb" {
   name               = "app-alb"
   load_balancer_type = "application"
+  internal           = false
+
   subnets = [
     var.public_subnet_1_id,
     var.public_subnet_2_id
   ]
+
   security_groups = [var.alb_sg_id]
+
+  tags = {
+    Name = "app-alb"
+  }
 }
 
+# -------------------------------
+# Target Group
+# -------------------------------
 resource "aws_lb_target_group" "tg" {
   name     = "app-tg"
   port     = 8000
@@ -21,8 +34,15 @@ resource "aws_lb_target_group" "tg" {
     healthy_threshold   = 2
     unhealthy_threshold = 3
   }
+
+  tags = {
+    Name = "app-tg"
+  }
 }
 
+# -------------------------------
+# Listener
+# -------------------------------
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 80
